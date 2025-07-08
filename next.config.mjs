600918409f77e4a66bd99d7eb2827dc7a228b0c3
nguyentam <tamnt@aladintech.co>
2025-07-08 23:00:10 +0700

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io'],
+    domains: ["utfs.io", "custome-case.vercel.app", "038ja5uyn7.ufs.sh"],
   },
 }
 
