cc9cf3d29174d3470dad7651bc7f6d0d9955876a
nguyentam <tamnt@aladintech.co>
2025-07-08 22:17:12 +0700

    update code

--- README.md
+++ README.md
@@ -30,7 +30,6 @@ To get started with this project, run
 
 and copy the .env.example variables into a separate .env file, fill them out & and that's all you need to get started!
 
-
 ## Acknowledgements
 
 - [Kinde](https://link.joshtriedcoding.com/kinde) for making this project possible
--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -1,10 +1,10 @@
-'use server'
+"use server"
 
-import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
-import { db } from '@/db'
-import { stripe } from '@/lib/stripe'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { Order } from '@prisma/client'
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products"
+import { db } from "@/db"
+import { stripe } from "@/lib/stripe"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { Order } from "@prisma/client"
 
 export const createCheckoutSession = async ({
   configId,
@@ -16,21 +16,21 @@ export const createCheckoutSession = async ({
   })
 
   if (!configuration) {
-    throw new Error('No such configuration found')
+    throw new Error("No such configuration found")
   }
 
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
   if (!user) {
-    throw new Error('You need to be logged in')
+    throw new Error("You need to be logged in")
   }
 
   const { finish, material } = configuration
 
   let price = BASE_PRICE
-  if (finish === 'textured') price += PRODUCT_PRICES.finish.textured
-  if (material === 'polycarbonate')
+  if (finish === "textured") price += PRODUCT_PRICES.finish.textured
+  if (material === "polycarbonate")
     price += PRODUCT_PRICES.material.polycarbonate
 
   let order: Order | undefined = undefined
@@ -57,10 +57,10 @@ export const createCheckoutSession = async ({
   }
 
   const product = await stripe.products.create({
-    name: 'Custom iPhone Case',
+    name: "Custom iPhone Case",
     images: [configuration.imageUrl],
     default_price_data: {
-      currency: 'USD',
+      currency: "USD",
       unit_amount: price,
     },
   })
@@ -68,9 +68,9 @@ export const createCheckoutSession = async ({
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ['card', 'paypal'],
-    mode: 'payment',
-    shipping_address_collection: { allowed_countries: ['DE', 'US'] },
+    payment_method_types: ["card"],
+    mode: "payment",
+    shipping_address_collection: { allowed_countries: ["DE", "US"] },
     metadata: {
       userId: user.id,
       orderId: order.id,
